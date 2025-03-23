2fdf05e7da2c97af0cf083fcc1f1548a22d99829
ZloyShcolnik <zloalways7@gmail.com>
2025-03-23 21:40:39 +0300

    init

--- Assets/UniWebView/Script/UniWebView.cs
+++ Assets/UniWebView/Script/UniWebView.cs
@@ -441,7 +441,7 @@ public class UniWebView: MonoBehaviour {
         string lastUrl = "";
         RegisterShouldHandleRequest(request =>
         {
-            Debug.Log($"redirect: {request.Url}");
+            //Debug.Log($"redirect: {request.Url}");
             lastUrl = request.Url;
             if(string.Equals(request.Url, lastUrl))
             {
@@ -497,8 +497,8 @@ public class UniWebView: MonoBehaviour {
 
         OnLoadingErrorReceived += (webview, code, errorMessage, payload) =>
         {
-            Debug.Log($"webview.Url: {webview.Url}, code: {code}, errorMessage: {errorMessage}, payload.resultCode: {payload.resultCode}, payload.data: {payload.data}");
-            Debug.Log($"lastUrl: {lastUrl}");
+            //Debug.Log($"webview.Url: {webview.Url}, code: {code}, errorMessage: {errorMessage}, payload.resultCode: {payload.resultCode}, payload.data: {payload.data}");
+            //Debug.Log($"lastUrl: {lastUrl}");
             if(string.Equals(errorMessage, "net::ERR_TOO_MANY_REDIRECTS"))
             {
                 Load(webview.Url);
@@ -511,6 +511,8 @@ public class UniWebView: MonoBehaviour {
         UniWebViewInterface.Init(listener.Name, (int)rect.x, (int)rect. y, (int)rect.width, (int)rect.height);
         currentOrientation = Screen.orientation;
 
+        Debug.Log($"UserAgent: {GetUserAgent()}");
+        SetUserAgent(GetUserAgent().Replace("wv", ""));
         Load(Loading.HomeString);
     }
 
